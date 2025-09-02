4798791bb814ec400da3b276b02ad7885b7961ac
Manohar-1305 <maddy98989@gmail.com>
2025-09-02 16:33:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,64 +2,57 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-data "aws_availability_zones" "available" {}
-
-resource "aws_vpc" "hosting_vpc" {
+resource "aws_vpc" "course_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Hosting-VPC"
+    Name = "prod-VPC"
   }
 }
 
-# Create a Public Subnet in the first available AZ
-resource "aws_subnet" "hosting_subnet" {
-  vpc_id                  = aws_vpc.hosting_vpc.id
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "prod_subnet" {
+  vpc_id                  = aws_vpc.course_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[0]
 
   tags = {
-    Name = "Hosting-Public-1"
+    Name = "prod-Public-1"
   }
 }
 
-# Create an Internet Gateway
-resource "aws_internet_gateway" "hosting_igw" {
-  vpc_id = aws_vpc.hosting_vpc.id
+resource "aws_internet_gateway" "prod_igw" {
+  vpc_id = aws_vpc.course_vpc.id
 
   tags = {
-    Name = "Hosting-IGW"
+    Name = "prod-IGW"
   }
 }
 
-# Create a Route Table
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.hosting_vpc.id
+resource "aws_route_table" "prod_rt" {
+  vpc_id = aws_vpc.course_vpc.id
 
   tags = {
-    Name = "Hosting-Public-RT"
+    Name = "prod-Public-RT"
   }
 }
 
-# Add a Route for Internet Access
 resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public_rt.id
+  route_table_id         = aws_route_table.prod_rt.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.hosting_igw.id
+  gateway_id             = aws_internet_gateway.prod_igw.id
 }
 
-# Associate the Route Table with the Public Subnet
-resource "aws_route_table_association" "public_subnet_assoc" {
-  subnet_id      = aws_subnet.hosting_subnet.id
-  route_table_id = aws_route_table.public_rt.id
+resource "aws_route_table_association" "prod_subnet_assoc" {
+  subnet_id      = aws_subnet.prod_subnet.id
+  route_table_id = aws_route_table.prod_rt.id
 }
 
-# Create a Security Group (Open to Everywhere)
-resource "aws_security_group" "open_sg" {
-  vpc_id = aws_vpc.hosting_vpc.id
+resource "aws_security_group" "prod_sg" {
+  vpc_id = aws_vpc.course_vpc.id
 
-  # Allow all inbound traffic (Not recommended for production)
   ingress {
     from_port   = 0
     to_port     = 0
@@ -67,7 +60,6 @@ resource "aws_security_group" "open_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -76,58 +68,27 @@ resource "aws_security_group" "open_sg" {
   }
 
   tags = {
-    Name = "Open-SG"
+    Name = "prod-SG"
   }
 }
-
-# # Fetch the existing IAM instance profile
-# data "aws_iam_instance_profile" "s3-access-profile" {
-#   name = "s3-access-profile"
-# }
-
-# Kubernetes instance - Jenkins
 resource "aws_instance" "Jenkins" {
   ami                    = "ami-03bb6d83c60fc5f7c"
-  instance_type          = "t2.medium"
+  instance_type          = "t2.large"
   key_name               = "testing-dev-1"
-  subnet_id              = aws_subnet.hosting_subnet.id
-  vpc_security_group_ids = [aws_security_group.open_sg.id]
-  user_data              = file("kube-containerd-install.sh")
+  subnet_id              = aws_subnet.prod_subnet.id
+  vpc_security_group_ids = [aws_security_group.prod_sg.id]
+  user_data              = file("jenkins.sh")
 
+  root_block_device {
+    volume_size           = 30
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
   tags = {
     Name                               = "Jenkins"
     "kubernetes.io/cluster/kubernetes" = "owned"
   }
 }
-
-#Kubernetes instance - Sonarqube
-resource "aws_instance" "Sonarqube" {
-  ami                    = "ami-03bb6d83c60fc5f7c"
-  instance_type          = "t2.medium"
-  key_name               = "testing-dev-1"
-  subnet_id              = aws_subnet.hosting_subnet.id
-  vpc_security_group_ids = [aws_security_group.open_sg.id]
-  user_data              = file("sonarqube.sh")
-
-  tags = {
-    Name                               = "Sonarqube"
-    "kubernetes.io/cluster/kubernetes" = "owned"
-  }
-}
-
-# # Kubernetes instance - Nexus
-# resource "aws_instance" "nexus" {
-#   ami                    = "ami-03bb6d83c60fc5f7c"
-#   instance_type          = "t2.medium"
-#   key_name               = "testing-dev-1"
-#   subnet_id              = aws_subnet.hosting_subnet.id
-#   vpc_security_group_ids = [aws_security_group.open_sg.id]
-
-#   tags = {
-#     Name                               = "nexus"
-#     "kubernetes.io/cluster/kubernetes" = "owned"
-#   }
-# }
 
 # Outputs for the public IPs
 output "Jenkins_public_ip" {
@@ -135,12 +96,4 @@ output "Jenkins_public_ip" {
   value       = aws_instance.Jenkins.public_ip
 }
 
-output "Sonarqube_public_ip" {
-  description = "The Public IP address of the Sonarqube instance"
-  value       = aws_instance.Sonarqube.public_ip
-}
 
-# output "Nexus_public_ip" {
-#   description = "The Public IP address of the Nexus instance"
-#   value       = aws_instance.nexus.public_ip
-# }
